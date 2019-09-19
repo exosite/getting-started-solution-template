78dbf6ca4ccb4da1b7ef029015d23bc60a5da589
Lena Wu <lenawu@exosite.com>
2019-09-19 14:01:34 +0800

    remove rate limit per endpoint

--- endpoints/example.lua
+++ endpoints/example.lua
@@ -15,27 +15,17 @@ return {id=request.parameters.userId}  -- json by default
 print("Fetch a given user" .. request.parameters.userId)
 return {id=request.parameters.userId}  -- json by default
 
---#ENDPOINT GET /api/ratelimit/withauth
+--#ENDPOINT GET /api/withauth
 --#TAGS security
 --#SECURITY bearer
---#RATELIMIT 1
 print("This endpoint needs a basic or bearer auth and " ..
 "has a rate limit 1 per minute, per authentication credentials. " ..
 "If no credentials is provided by user a 401 status code is returned.")
 return 'OK'
 
---#ENDPOINT GET /api/noauthnolimit
+--#ENDPOINT GET /api/noauth
 --#TAGS security
 --#SECURITY none
---#RATELIMIT 0
 print("Authentication and RateLimit are explicitly deactivated " ..
 "for this endpoint regardless of the API configuration.")
 return 'OK'
-
---#ENDPOINT GET /api/ratelimit
---#TAGS security
---#RATELIMIT 1
-print("No security defined, uses to the API default settings. " ..
-"If none defined, the ratelimit will be per the client Ip Address.")
-return 'OK'
--- oef
